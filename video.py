00d437a4079f0ef0e6880eedfa33330ed2f435a7
sam <github@sampalmer.dev>
2020-02-18 22:54:30 +0000

    Attempt to remove outliers

--- video.py
+++ video.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 import cv2
 from operator import itemgetter
+import pylab
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--radius", type = int)
@@ -55,6 +56,32 @@ def get_max_loc(gray):
     (_,_,_, max_loc) = cv2.minMaxLoc(gray)
     return max_loc
 
+def remove_outliers(arr):
+    print(arr)
+    coords = np.array(arr)
+
+    mean = np.mean(coords, axis=0)
+    sd = np.std(coords, axis=0)
+
+    x_list = [x for x in arr if ((x[0] + x[1] > mean - 5 * sd)).any()]
+    return [x for x in x_list if ((x[0] + x[1] < mean + 5 * sd)).any()]
+
+def force_same_length(x_arr, y_arr):
+    if len(x_arr) > len(y_arr):
+        return x_arr[0:len(y_arr)], y_arr
+    else:
+        return x_arr, y_arr[0:len(x_arr)]
+            
+
+def draw_line(arr):
+    no_outliers = remove_outliers(arr)
+    x_arr = [x[0] for x in no_outliers]
+    y_arr = [x[1] for x in no_outliers]
+
+    pylab.plot(x_arr, y_arr, 'o', color='k')
+    pylab.show()
+
+
 def readFrames():
     movingArr = []
     trace_arr = []
@@ -65,13 +92,15 @@ def readFrames():
         gray = adjust_gamma(gray, args["gamma"])
         max_loc = get_max_loc(gray)
         trace_arr.append(max_loc)
-        if is_moving(trace_arr):
+        if len(trace_arr) > 100 and is_moving(trace_arr):
+            draw_line(trace_arr)
             direction = get_direction(trace_arr)
+
             print(direction)
             trace_arr = []
             
         cv2.circle(gray, max_loc, radius, (255, 0, 0), 2)
-        cv2.imshow("Robust", gray)
+        #cv2.imshow("Robust", gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
